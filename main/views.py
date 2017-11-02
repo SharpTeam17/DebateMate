4c01cf4e67ec998531896b780b144b4fb7483f38
Triads <austin.nabors@yahoo.com>
2017-11-01 22:41:11 -0500

    Adding the home page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def home(request):
+    debate_feed = {}
+    template = loader.get_template('main/home.html')
+    context = {
+        'debate_feed': debate_feed,
+    }
+    return HttpResponse(template.render(context, request))
