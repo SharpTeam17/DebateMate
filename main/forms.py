5bee8bdbd1fee1c84d8861d2d20338c06fe3cb68
bbrown683 <bbrown683@live.com>
2017-11-14 15:53:20 -0600

    Adding Side selection.

--- main/forms.py
+++ main/forms.py
@@ -3,15 +3,30 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field
 
 class JoinForm(forms.Form):
+    SPECTATOR = 'S'
+    DEBATOR = 'D'
+    MODERATOR = 'M'
+
     role = forms.ChoiceField(
         choices = (
-            ('S', 'Spectator'),
-            ('D', 'Debator'),
-            ('M', 'Moderator')
+            (SPECTATOR, 'Spectator'),
+            (DEBATOR, 'Debator'),
+            (MODERATOR, 'Moderator')
         ),
         widget = forms.RadioSelect,
-        label ='Select a Role',
+        label = 'Select a Role',
         initial = 'S',
+    )
+
+    side = forms.ChoiceField(
+        choices = 
+        (
+            ('A', 'A'),
+            ('B', 'B'),
+        ),
+        widget = forms.RadioSelect,
+        label = 'Select a Side',
+        required = False,
     )
 
     helper = FormHelper()
@@ -19,6 +34,12 @@ class JoinForm(forms.Form):
     helper.add_input(Submit('join', 'Join', css_class="btn-primary"))
     helper.add_input(Submit('cancel', 'Cancel', css_class="btn-secondary"))
 	
+    def __init__(self, data=None, *args, **kwargs):
+        super(JoinForm, self).__init__(data, *args, **kwargs)
+
+        if data and data.get('role') == self.DEBATOR:
+            self.fields['side'].required = True
+
 class TopicForm(forms.Form):
     topic = forms.CharField(label = 'Debate topic:', required = True)
 	
